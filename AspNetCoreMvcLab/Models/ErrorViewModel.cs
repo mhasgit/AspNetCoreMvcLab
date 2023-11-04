44ab252afc2439dee71a53a531ec879155351b7a
mhasgit <clumsy7674@gmail.com>
2023-11-04 16:50:57 +0500

    initialize Mvc Project

---
+++ AspNetCoreMvcLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreMvcLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
